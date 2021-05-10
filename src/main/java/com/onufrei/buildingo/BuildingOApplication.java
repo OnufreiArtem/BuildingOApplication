c547933992586faca24f0a732b4eee7f4ab6ef45
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-10 18:05:53 +0300

    Initial commit

---
+++ src/main/java/com/onufrei/buildingo/BuildingOApplication.java
@@ -0,0 +1,13 @@
+package com.onufrei.buildingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildingOApplication.class, args);
+    }
+
+}
