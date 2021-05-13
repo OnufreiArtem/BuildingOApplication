175e05dd936f10a707c2b4a7ddd9bfba36b27601
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-13 10:47:00 +0300

    Changed type of specification from EmployeeSpecification to BrigadeSpecification and added swagger descriptions for the field

--- src/main/java/com/onufrei/buildingo/model/Brigade.kt
+++ src/main/java/com/onufrei/buildingo/model/Brigade.kt
@@ -1,5 +1,6 @@
 package com.onufrei.buildingo.model
 
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
@@ -7,13 +8,21 @@ import java.util.*
 
 @Document
 data class Brigade(
+        @ApiModelProperty(notes="The id of the brigade in UUID format.")
         @Id
         val id: String = UUID.randomUUID().toString(),
+        @ApiModelProperty(notes="The name of the brigade.")
         var name: String,
+        @ApiModelProperty(notes="The description of the brigade.")
         var description: String = "",
+        @ApiModelProperty(notes="The chief of the brigade.")
         var chief: Employee,
-        var specification: EmployeeSpecification,
+        @ApiModelProperty(notes="The specification of brigade.")
+        var specification: BrigadeSpecification,
+        @ApiModelProperty(notes="The state of the brigade. If isActive is true - the brigade is able do their work, otherwise - brigade is not able.")
         var isActive: Boolean,
+        @ApiModelProperty(notes="The date and time when object was created.")
         val createdAt: LocalDateTime = LocalDateTime.now(),
+        @ApiModelProperty(notes="The date and time when object was lastly modified.")
         var modifiedAt: LocalDateTime = LocalDateTime.now()
 )
