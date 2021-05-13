0b35b967c050b4ef6bfa563bbb2edc4131668ece
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-13 10:46:02 +0300

    Removed contract and added swagger descriptions

--- src/main/java/com/onufrei/buildingo/model/Plot.kt
+++ src/main/java/com/onufrei/buildingo/model/Plot.kt
@@ -1,5 +1,6 @@
 package com.onufrei.buildingo.model
 
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
@@ -7,11 +8,15 @@ import java.util.*
 
 @Document
 data class Plot(
+        @ApiModelProperty(notes="The id of the plot in UUID format.")
         @Id
         val id: String = UUID.randomUUID().toString(),
+        @ApiModelProperty(notes="The address of the plot.")
         var address: String,
-        var contract: Contract,
+        @ApiModelProperty(notes="The chief of the plot.")
         var chief: Employee,
+        @ApiModelProperty(notes="The date and time when object was created.")
         val createdAt: LocalDateTime = LocalDateTime.now(),
+        @ApiModelProperty(notes="The date and time when object was lastly modified.")
         var modifiedAt: LocalDateTime = LocalDateTime.now()
 )
