b5481d2bd5c1ed51c4024905ed8e2dcc3578c028
Artem Onufrei <2hilderoy2@gmail.com>
2021-05-13 10:50:59 +0300

    Added swagger description for fields

--- src/main/java/com/onufrei/buildingo/model/Building.kt
+++ src/main/java/com/onufrei/buildingo/model/Building.kt
@@ -1,17 +1,26 @@
 package com.onufrei.buildingo.model
 
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import java.util.*
 
 @Document
 data class Building(
+        @ApiModelProperty(notes="The id of the building in UUID format.")
         @Id
         val id: String = UUID.randomUUID().toString(),
+        @ApiModelProperty(notes="The plot where building it is situated.")
+        @DBRef
         var plot: Plot,
+        @ApiModelProperty(notes="The plan of the building.")
         var plan: String,
+        @ApiModelProperty(notes="The chief of the building of the building.")
         var chief: Employee,
+        @ApiModelProperty(notes="The date and time when object was created.")
         val createdAt: LocalDateTime = LocalDateTime.now(),
+        @ApiModelProperty(notes="The date and time when object was lastly modified.")
         var modifiedAt: LocalDateTime = LocalDateTime.now()
 )
--- src/main/java/com/onufrei/buildingo/model/Estimate.kt
+++ src/main/java/com/onufrei/buildingo/model/Estimate.kt
@@ -1,5 +1,6 @@
 package com.onufrei.buildingo.model
 
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
@@ -7,9 +8,13 @@ import java.util.*
 
 @Document
 data class Estimate(
+        @ApiModelProperty(notes="The id of the estimate in UUID format.")
         @Id
         val id: String = UUID.randomUUID().toString(),
+        @ApiModelProperty(notes="The plot that is linked to estimate.")
         var plot: Plot,
+        @ApiModelProperty(notes="The date and time when object was created.")
         val createdAt: LocalDateTime = LocalDateTime.now(),
+        @ApiModelProperty(notes="The date and time when object was lastly modified.")
         var modifiedAt: LocalDateTime = LocalDateTime.now()
 )
