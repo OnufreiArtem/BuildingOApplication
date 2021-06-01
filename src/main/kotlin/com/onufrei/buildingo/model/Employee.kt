bea142cdb2bfe7b012f5d1dc5167439ca48e329b
Artem Onufrei <2hilderoy2@gmail.com>
2021-06-01 12:23:48 +0300

    Tried to solve problem is employees

--- src/main/kotlin/com/onufrei/buildingo/model/Employee.kt
+++ src/main/kotlin/com/onufrei/buildingo/model/Employee.kt
@@ -2,6 +2,7 @@ package com.onufrei.buildingo.model
 
 import io.swagger.annotations.ApiModelProperty
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.DBRef
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
